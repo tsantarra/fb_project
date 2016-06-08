517b2aa1a4e5aa2d82ef74ec753bfc4c252e5ab1
Trevor Sarratt <tsarratt@soe.ucsc.edu>
2016-06-08 16:44:28 -0700

    Added audio stream capability via python sounddevice. Also possible via pyaudio.

--- data_sources.py
+++ data_sources.py
@@ -18,7 +18,22 @@ def get_wav_frames_from_file(filename):
 
 # STREAMING AUDIO
 # https://people.csail.mit.edu/hubert/pyaudio/docs/#pyaudio-documentation
+# http://people.csail.mit.edu/hubert/pyaudio/     -> see "record" example
 # http://stackoverflow.com/questions/4160175/detect-tap-with-pyaudio-from-live-mic/4160733#4160733
+
+# http://python-sounddevice.readthedocs.io/en/0.3.3/#device-selection
+
+# What sounddevice is based on: https://github.com/bastibe/PySoundCard/
+
+
+def get_audio_from_stream():
+    """ This appear to fit the desired generator style well!"""
+    import sounddevice as sd
+    with sd.InputStream(device=0, channels=1, latency='low') as stream:
+        while True:  # Need break case
+            data, flag = stream.read(stream.read_available)
+            yield data
+
 
 
 def get_video_frames_from_file(vid_filename):
